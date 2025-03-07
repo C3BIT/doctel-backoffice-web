f484c44810439cbed557dab40ce3d8647da50625
Nahid Hasan <nahidjc80@gmail.com>
2025-03-07 23:17:08 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import NotFound from './pages/NotFound/NotFound';
 import OtpVerify from './pages/auth/OtpVerify';
 import Home from './pages/Home/Home';
 const App = () => {
-<<<<<<< HEAD
   return (
     <Box className="App">
         <Routes>
@@ -22,23 +21,5 @@ const App = () => {
         </Routes>
     </Box>
   );
-=======
-    return (
-        <div className='app'>
-            <Router>
-                <Routes>
-                    <Route element={<ProtectedRoute />}>
-                        <Route path="/" element={<Layout />}>
-                            <Route index element={<Home />} />
-                        </Route>
-                    </Route>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/verify-otp" element={<OtpVerify />} />
-                    <Route path="*" element={<Layout><NotFound /></Layout>} />
-                </Routes>
-            </Router>
-        </div>
-    );
->>>>>>> ce7037a7e28dd4f0da06e60e08876644fd9eb0b7
 };
 export default App;
