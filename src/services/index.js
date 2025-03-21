55fae9969c9ed47b380cc144677cffc1bd40189a
Nahid Hasan <nahidjc80@gmail.com>
2025-03-21 06:00:32 +0600

    implement UI logic to end the meeting when the other party disconnects

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,3 @@
-export const api = "https://api.bloomattires.com/api";
+export const api = "http://192.168.0.244:8088/api";
 export const socketURL= import.meta.env.VITE_SOCKET_URL
 export const environment = import.meta.env.VITE_ENVIRONMENT
