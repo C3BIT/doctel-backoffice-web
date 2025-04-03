ad7b2f8180fac1bdbf5e07777e6f304601d26613
Masud Rana <masud15-924@diu.edu.bd>
2025-04-03 17:56:39 +0600

    API implement in prescription

--- src/redux/prescription/prescriptionSlice.jsx
+++ src/redux/prescription/prescriptionSlice.jsx
@@ -5,7 +5,7 @@ export const createPrescription = createAsyncThunk(
     "prescriptions/createPrescription",
     async ({ token, formData }, { rejectWithValue }) => {
       try {
-        const response = await privatePutFile("/doctors/upload/prescription", token, formData);
+        const response = await privatePost("/doctors/upload/prescription", token, formData);
         return response.data;
       } catch (err) {
         return rejectWithValue(
